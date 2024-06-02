2576830ef224f4a2a39e597e9c55ceb400218428
YukihiraDeve <valentinbourpro@gmail.com>
2024-06-03 01:48:43 +0200

    Export test

--- Backend/scripts/exportMap.sh
+++ Backend/scripts/exportMap.sh
@@ -14,17 +14,15 @@ worldPath="/servers/$serverName/$worldName"
 outputDir="/servers/$serverName/exports"
 outputFile="$outputDir/$worldName.obj"
 
-VENV_DIR="$scriptDir/../export/env"
+VENV_DIR="$scriptDir/../env"
 
 source "$VENV_DIR/bin/activate"
-
 
 getSpawnCoordsScript="$scriptDir/../export/SpawnCoordinate.py"
 
 mkdir -p "$outputDir"
 
 spawnCoords=$(python3 "$getSpawnCoordsScript" "$worldPath/playerdata")
-
 
 if [ -z "$spawnCoords" ]; then
   echo "[ERROR] Unable to retrieve player spawn coordinates."
@@ -36,15 +34,16 @@ read spawnX spawnY spawnZ <<< "$spawnCoords"
 
 echo "Spawn Coordinates: x=$spawnX, y=$spawnY, z=$spawnZ"
 
-
 java -jar "$jmc2objPath" --resource-pack="$MinecraftJarPath" "$worldPath" -o "$outputFile" --area="$spawnX,$spawnY,$spawnZ,$((spawnX+512)),$((spawnZ+512))"
 
 sleep 10
 
 if [ $? -eq 0 ]; then
     echo "[INFO Model] Exportation réussie : $outputFile"
+    deactivate  # Désactiver l'environnement virtuel
     exit 0
 else
   echo "[ERROR Model] Erreur lors dans le modeling" >&2
+  deactivate  # Désactiver l'environnement virtuel
   exit 1
 fi
